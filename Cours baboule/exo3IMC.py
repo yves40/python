67863ad073ccee954c1a2347aa94015d2bb0f85e
yves40 <yves.toubhans@free.fr>
2022-01-02 18:13:31 +0100

    What a program !!

--- Cours baboule/exo3IMC.py
+++ Cours baboule/exo3IMC.py
@@ -13,9 +13,9 @@ print ' Extra   si : POIDS < 18.5'
 print ' Normal  si : 18.5 < POIDS < 25'
 print ' Mauvais si : POIDS > 18.5'
 print '-----------------------------------------------'
-sys.stdout.write('Entrez votre taille : ')
+sys.stdout.write('Entrez votre taille (m) : ')
 TAILLE = sys.stdin.readline()
-sys.stdout.write('Entrez votre poids : ')
+sys.stdout.write('Entrez votre poids (kg ): ')
 POIDS = sys.stdin.readline()
 TAILLE = float(TAILLE)
 IMC = int(POIDS) / (TAILLE*TAILLE)
